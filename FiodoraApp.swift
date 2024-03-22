713fb24a62edbe97da559c3df863874ab45ae2d4
Juan Latorre <juanlatorreharcha@gmail.com>
2024-03-22 19:36:37 -0300

    empty app

---
+++ FiodoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiodoraApp.swift
+//  Fiodora
+//
+//  Created by Juan Latorre on 22-03-24.
+//
+
+import SwiftUI
+
+@main
+struct FiodoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
