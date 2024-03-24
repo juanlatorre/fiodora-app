a4fe4be8ca191ce0056cb90a928c438f61ca83c9
Juan Latorre <juanlatorreharcha@gmail.com>
2024-03-24 10:44:03 -0300

    basic app with graphql config

---
+++ Fiodora/Fiodora/FiodoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  FiodoraApp.swift
+//  Fiodora
+//
+//  Created by Juan Latorre on 24-03-24.
+//
+
+import SwiftUI
+
+@main
+struct FiodoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
